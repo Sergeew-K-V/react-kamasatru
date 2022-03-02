e80f8a47b10b8ffb7008d36d9a92607341b1bb89
Kirill <sergeew_k@mail.ru>
2022-03-02 11:34:03 +0300

    need create block for workflow

---
+++ src/Components/Main/BuildUp/BuildUp.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../../../css/BuildUp.css'
+
+function BuildUp() {
+  return (
+    <section>
+      <div className='container'>
+        <div className='build-up__block'>
+          <div className='build-up__top'>
+            <div className='container-smallest'>
+              <h2 className='build-up__title title-big'>
+                Build up the whole picture
+              </h2>
+              <p>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum — semper quis
+                lectus nulla at volutpat diam ut venenatis.
+              </p>
+            </div>
+          </div>
+          <div className='build-up__bottom'></div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default BuildUp
---
+++ src/Components/Main/BuildUp/RobustWorkflow/RobustWorkflow.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../../../../css/RobustWorkflow.css'
+
+function RobustWorkflow() {
+  return (
+    <div className='Robust__block'>
+      <div className='Robust__flex'>
+        <div className='Robust__top'>
+          <img src='' alt='picture workflow' />
+        </div>
+        <div className='Robust__bottom'>
+          <h4 className='Robust__title title'>Robust Workflow</h4>
+          <p className='Robust__text text'>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RobustWorkflow
