502902f242f321c32fa4b28c9a82e8e07ad2ad0b
Kirill <sergeew_k@mail.ru>
2022-03-02 18:14:59 +0300

    3 block

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,14 @@
 import react from 'react'
 import BuildUp from './BuildUp/BuildUp'
 import LandingTemplate from './LandingTemplate/LandingTemplate'
+import Workflow from './Workflow/Workflow'
 
 function Main() {
   return (
     <main className='main-content'>
       <LandingTemplate></LandingTemplate>
       <BuildUp></BuildUp>
+      <Workflow></Workflow>
     </main>
   )
 }
---
+++ src/Components/Main/Workflow/Workflow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../../css/Workflow.css'
+
+function Workflow() {
+  return (
+    <section>
+      <div></div>
+    </section>
+  )
+}
+
+export default Workflow
