5502270abfcad60d5b4043f3bbe5e9b59d37647a
Kirill <sergeew_k@mail.ru>
2022-03-04 18:57:24 +0300

    finish workflow and next block customers

---
+++ src/Components/Main/Workflow/DataDriven/DataDriven.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import '../../../../css/DataDriven.css'
+
+function DataDriven({ count, image }) {
+  if (count % 2 !== 0) {
+    return (
+      <div className='datadriven'>
+        <div className='datadriven__block'>
+          <div className='datadriven__left_notzero'>
+            <div className='left__top before-top'>LIGHTNING FAST WORKFLOW</div>
+            <h3 className='left__middle'>Data-driven insights</h3>
+            <p className='left__text text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className='datadriven__right_notzero'>
+            <img src={image} alt='workflow-image' />
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='datadriven'>
+        <div className='datadriven__block'>
+          <div className='datadriven__left_zero'>
+            <img src={image} alt='workflow-image' />
+          </div>
+          <div className='datadriven__right_zero'>
+            <div className='right__top before-top'>LIGHTNING FAST WORKFLOW</div>
+            <h3 className='right__middle'>Data-driven insights</h3>
+            <p className='right__text text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua — Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DataDriven
--- src/Components/Main/Workflow/Workflow.js
+++ src/Components/Main/Workflow/Workflow.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import '../../../css/Workflow.css'
+import DataDriven from './DataDriven/DataDriven'
+import workFlow_1 from '../../../img/Workflow/workflow_1.png'
+import workFlow_2 from '../../../img/Workflow/workflow_2.png'
+import workFlow_3 from '../../../img/Workflow/workflow_3.png'
 
 function Workflow() {
   return (
-    <section>
-      <div></div>
+    <section className='workflow'>
+      <div className='container'>
+        <div className='workflow__block'>
+          <div className='workflow__top'>
+            <div className='container-smallest'>
+              <h2 className='workflow__title title-middle'>Workflow that just works</h2>
+              <p className='workflow__text text'>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum — semper quis lectus nulla at volutpat diam ut venenatis.
+              </p>
+            </div>
+          </div>
+          <div className='workflow__bottom'>
+            <div className='bottom__block'>
+              <DataDriven count={1} image={workFlow_1}></DataDriven>
+              <DataDriven count={2} image={workFlow_2}></DataDriven>
+              <DataDriven count={3} image={workFlow_3}></DataDriven>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
