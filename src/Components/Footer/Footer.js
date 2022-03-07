1723c351c3b8cd577af222b2215fc4d03e32adcc
Kirill <sergeew_k@mail.ru>
2022-03-07 19:05:32 +0300

    footer next css footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,51 @@
 import React from 'react'
 
 function Footer() {
-  return <footer>footer</footer>
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <div className='footer__block'>
+          <div className='flex__top'>
+            <div className='top__brand'></div>
+            <div className='top__social'>
+              <ul className='top__icons'>
+                <li className='top__item'>
+                  <a href=''></a>
+                </li>
+                <li className='top__item'>
+                  <a href=''></a>
+                </li>
+                <li className='top__item'>
+                  <a href=''></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='flex__bottom'>
+            <div className='bottom__copyright'>
+              Made by <span className='bottom__nickname'>Fobelend</span>. All right reserved
+            </div>
+          </div>
+          <nav className='bottom__nav'>
+            <ul>
+              <li className='nav__item'>
+                <a href=''>Contact</a>
+              </li>
+              <li className='nav__item'>
+                <a href=''>About us</a>
+              </li>
+              <li className='nav__item'>
+                <a href=''>FAQ's</a>
+              </li>
+              <li className='nav__item'>
+                <a href=''>Support</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
