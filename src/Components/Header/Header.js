5e76cec0f95b6c3c8d009fd65b78ada9836c6767
Kirill <sergeew_k@mail.ru>
2022-03-11 18:50:32 +0300

    created animation for burger

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDoorClosed } from '@fortawesome/free-solid-svg-icons'
 import navbarLogo from '../../img/Header/navbarLogo.svg'
 import '../../css/Header.css'
 
 function Header() {
+  const [navbarActive, setNavbarActive] = useState(false)
+
   return (
     <header className='navbar'>
       <div className='container'>
@@ -12,11 +14,31 @@ function Header() {
           <a href='#' className='navbar__link_logo navbar__item'>
             <img src={navbarLogo} alt='icon' className='navbar__logo' />
           </a>
-          <nav className='navbar__navigation navbar__item'>
-            <a className='navbar__link navigation__item'>Documentation</a>
-            <button className='navbar__btn navigation__item'>
-              <a className='btn__content '>Sign up</a>
-            </button>
+          <button
+            className='navbar__btn-burger'
+            onClick={() => {
+              setNavbarActive(!navbarActive)
+            }}
+          >
+            <span></span>
+          </button>
+          <nav
+            className={
+              navbarActive
+                ? 'navbar__navigation navbar__item active'
+                : 'navbar__navigation navbar__item'
+            }
+          >
+            <div className='navbar__block-for-mob'>
+              <div className='navbar__for-mob-top'>
+                <a className='navbar__link navigation__item'>Documentation</a>
+              </div>
+              <div className='navbar__for-mob-bottom'>
+                <button className='navbar__btn navigation__item'>
+                  <a className='btn__content'>Sign up</a>
+                </button>
+              </div>
+            </div>
           </nav>
         </div>
       </div>
